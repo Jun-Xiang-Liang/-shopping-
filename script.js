ee2356ceb5dddeb500f2bdd0efa0fea3f958606e
Andy_liang <andy0917298968@gmail.com>
2023-06-05 13:49:39 +0800

    success

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function(){
+
+    $("#collapse").on("click",function(){
+
+        $("#siderbar").toggleClass("active");
+     
+    })
+})
+
+
+for( let img of document.querySelectorAll("img")){
+    img.addEventListener("click",() => {
+        let container
+
+    })
+
+}
